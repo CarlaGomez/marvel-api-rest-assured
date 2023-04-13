445b7181b43fa7358d6f0fec1c307e3a7ab79519
Carla Gomez <c.gomez@zoovu.com>
2023-04-13 09:43:53 -0400

    update dotenv dependency

--- src/main/java/helpers/BaseClass.java
+++ src/main/java/helpers/BaseClass.java
@@ -4,19 +4,31 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class BaseClass {
 
     public static String timestamp() {
-        Dotenv dotenv = Dotenv.load();
+        Dotenv dotenv = Dotenv.configure()
+                .directory(".")
+                .ignoreIfMalformed() //
+                .ignoreIfMissing()
+                .load();
         String timestamp = dotenv.get("TIMESTAMP");
         return timestamp;
     }
 
     public static String publicKey() {
-        Dotenv dotenv = Dotenv.load();
+        Dotenv dotenv = Dotenv.configure()
+                .directory(".")
+                .ignoreIfMalformed() //
+                .ignoreIfMissing()
+                .load();
         final String publicKey = dotenv.get("PUBLIC_KEY");
         return publicKey;
     }
 
     public static String hash() {
-        Dotenv dotenv = Dotenv.load();
+        Dotenv dotenv = Dotenv.configure()
+                .directory(".")
+                .ignoreIfMalformed() //
+                .ignoreIfMissing()
+                .load();
         final String hash = dotenv.get("HASH");
         return hash;
     }
